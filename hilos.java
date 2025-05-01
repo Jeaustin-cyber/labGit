a9fb6f9cd34770479429b77d70f143b2e054b985
Jeaustin Morales Hidalgo <jeausti222@gmail.com>
2025-04-30 19:40:54 -0600

    CorregirCambio
    se corrigen los cambios que se hicieron al codigo

--- hilos.java
+++ hilos.java
@@ -1,3 +1,5 @@
-Lboratorio de gitHub
+//Laboratorio de gitHub
+  static BufferedReader Entrada = new BufferedReader(new InputStreamReader(System.in)); 
 String palabra;
 system.out.prinlt ("Hola este es mi primer cambio que voy a subir a la nube");
+palabra= entrada.readLine();
